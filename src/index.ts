58685fcd99c45fbf04010358ce35b6e5722761e2
Rafael Floriani Pinto <rafaelflorianipinto@gmail.com>
2022-01-05 17:04:17 -0300

    Exported Engine

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { sanitizeEmail } from "./sanitizeEmail.js";
 import {queryProtector} from "./sql.js";
-import {justCharsAndNumbers} from './justCharsAndNumbers'
+import {justCharsAndNumbers} from './justCharsAndNumbers';
+import {SanitizerEngine} from './engine';
 module.exports={
     email:sanitizeEmail,
     sqlQueryProtector:queryProtector,
     justCharsAndNumbers:justCharsAndNumbers,
+    engine:SanitizerEngine,
 }
